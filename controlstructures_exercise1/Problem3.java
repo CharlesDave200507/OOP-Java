5f391db681aa30a26d321ea2d2723bd66ee83aa5
CharlesDave200507 <alvarinocharlesdave@gmail.com>
2025-08-20 14:55:47 +0800

    Add files via upload

---
+++ controlstructures_exercise1/Problem3.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basic.controlstructures_exercise1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Problem3 {
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+        System.out.print("Input a 1st number:  ");
+	  int firstnum = sc.nextInt();
+            System.out.print("Input a 2nd number:  ");
+            int secondnum = sc.nextInt();
+            System.out.print("Input a 3rd number:  ");
+	  int thirdnum = sc.nextInt();
+          
+        int greatest = firstnum;
+        if (secondnum > greatest) {
+            greatest = secondnum;
+        } 
+        if (thirdnum > greatest) {
+            greatest = thirdnum;
+        }
+        System.out.println("The Greatest: "+ greatest);
+    }
+          
+          
+    }
+
